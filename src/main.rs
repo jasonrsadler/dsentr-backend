adea544219831b9534b3d8775336909b1f1ff2a2
Jason Sadler <25326067+jasonrsadler@users.noreply.github.com>
2025-05-14 19:29:01 +0000

    Changes to correct errors in main.rs

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,9 @@ use axum::{
         Response, IntoResponse
     }
 };
-use http::Method;
-use http::header::{AUTHORIZATION, CONTENT_TYPE};
-use http::HeaderValue;
+use axum::http::Method;
+use axum::http::header::{AUTHORIZATION, CONTENT_TYPE};
+use axum::http::HeaderValue;
 use responses::JsonResponse;
 use sqlx::PgPool;
 use tokio::net::TcpListener;
@@ -21,7 +21,6 @@ use std::{sync::Arc, net::SocketAddr};
 use tracing::{info, Level};
 use tracing_subscriber::FmtSubscriber;
 use tower_http::cors::CorsLayer;
-use tower::ServiceBuilder;
 use config::Config;
 use routes::early_access::handle_early_access;
 use routes::auth::{handle_login, handle_signup, verify_email};
@@ -48,7 +47,7 @@ async fn main() {
     };
 
     let cors = CorsLayer::new()
-        .allow_origin(config.frontend_origin.parse::<HeaderValue>().expect("Invalid FRONTEND_ORIGIN"))
+        .allow_origin(config.frontend_origin.parse::<HeaderValue>().unwrap())
         .allow_methods([Method::GET, Method::POST])
         .allow_headers([AUTHORIZATION, CONTENT_TYPE])
         .allow_credentials(true);
@@ -59,14 +58,14 @@ async fn main() {
         .route("/api/auth/signup", post(handle_signup))
         .route("/api/auth/login", post(handle_login))
         .route("/api/auth/verify", post(verify_email))
-        .with_state(state);
+        .with_state(state)
+        .layer(cors);
 
+    let make_service = app.into_make_service();
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     let listener = TcpListener::bind(addr).await.unwrap();
 
-    axum::serve(addr, ServiceBuilder::new()
-            .layer(cors)
-            .service(app.into_make_service()))
+    axum::serve(listener, make_service)
         .await
         .unwrap();
 }
