7985ddd7237803ca1ffdfb93bd4d7feddddea087
Jason Sadler <jason.r.sadler@gmail.com>
2025-06-02 15:23:41 -0400

    Added tests for logout and session

--- src/routes/auth/claims.rs
+++ src/routes/auth/claims.rs
@@ -2,15 +2,15 @@ use serde::{Deserialize, Serialize};
 
 use crate::models::user::UserRole;
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
 pub struct Claims {
-    pub id: String,     // user ID or UUID
+    pub id: String, // user ID or UUID
     pub email: String,
-    pub exp: usize,      // expiration (as UNIX timestamp)
+    pub exp: usize, // expiration (as UNIX timestamp)
     pub first_name: String,
     pub last_name: String,
     // Optional fields
     pub role: Option<UserRole>,
     pub plan: Option<String>,
-    pub company_name: Option<String>
+    pub company_name: Option<String>,
 }
